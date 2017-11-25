03b290e1eea63439d79da52adcdc3a585df69903
MasterLolicon <hernandez_pm13004025@hotmail.com>
2017-11-25 15:31:10 -0600

    pagina de entrenamiento para usar HTML y CSS

---
+++ paginas de entrenamiento/ejemplo php.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Ejemplo php</title>
+<meta charset="Unicode">
+</head>
+<h1>ejemplo de textos o cualquier cosa posible en php</h1>
+<br><br>
+<?php
+echo "parte en php como principiante :v ";
+?>
+<br>
+<body>
+</body>
+</html>
